9c63a8135a6d194544fd8e39fe9a3f60af6c1b54
Pablo Quemé <pgqueme@gmail.com>
2017-06-06 11:26:01 -0600

    Improving README and cleaning code

--- index.js
+++ index.js
@@ -1,66 +1,40 @@
-//var fs = require('fs');
 var pdf = require('html-pdf');
-var options = { format: 'Letter', phantomPath: './phantomjs_lambda/phantomjs_linux-x86_64' };
-
-//var MemoryStream = require('memorystream'); //Maybe won't use
 var AWS = require('aws-sdk');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
+var options = { format: 'Letter', phantomPath: './phantomjs_lambda/phantomjs_linux-x86_64' };
+var S3config = { bucketName: 'your-bucket' }; //Change to your bucket name
+
 exports.handler = function(event, context) {
-  /*var memStream = new MemoryStream();
-  var html_utf8 = new Buffer(event.html_base64, 'base64').toString('utf8');
-
-  wkhtmltopdf(html_utf8, event.options, function(code, signal) {
-    var pdf = memStream.read();
-    var s3 = new AWS.S3();
-
-    var params = {
-      Bucket : "my-bucket",
-      Key : "test.pdf",
-      Body : pdf
-    }
-
-    s3.putObject(params, function(err, data) {
-      if (err) {
-        console.log(err)
-      } else {
-        context.done(null, { pdf_base64: pdf.toString('base64') });
-      }
-    });
-  }).pipe(memStream);*/
-
-
+  //Get the values from the request
   var htmlString = event.htmlString;
   var fileName = event.fileName;
 
-  //pdf.create(htmlString, options).toFile("../public/archivos/" + fileName + ".pdf", function(err, resFile) {
+  //Create the PDF file from the HTML string
   pdf.create(htmlString, options).toBuffer(function(err, buffer){
-      if (err) return console.log(err);
-      var s3 = new AWS.S3();
-      var params = {
-          Bucket : "lexglobal-pdf",
-          Key : fileName + '.pdf',
-          Body : buffer
+      if (err){
+        console.log(err);
+        var error = new Error("There was an error generating the PDF file");
+        callback(error);
       }
+      else {
+        var s3 = new AWS.S3();
+        var params = {
+            Bucket : S3config.bucketName,
+            Key : fileName + '.pdf',
+            Body : buffer
+        }
 
-      s3.putObject(params, function(err, data) {
-          if (err) {
-              console.log(err)
-          } else {
-              context.done(null, { result: 'Created PDF file' });
-          }
-      });
+        s3.putObject(params, function(err, data) {
+            if (err) {
+                console.log(err);
+                var error = new Error("There was an error while saving the PDF to S3");
+                callback(error);
+            } else {
+                context.done(null, { result: 'Created PDF file' });
+            }
+        });
+      }
   });
-
-  /*console.log('Archivo HTML creado');
-  var html = fs.readFileSync("../public/archivos/" + fileName + ".html", 'utf8');
-  console.log('Archivo HTML leído');
-  
-  pdf.create(html, options).toFile("../public/archivos/" + fileName + ".pdf", function(err, resFile) {
-      if (err) return console.log(err);
-      console.log("PDF guardado");
-      res.status(201).send({ filePath: resFile });
-      console.log('Ruta PDF enviada');
-  });*/
 };
